2c9e974323ff401d70c763c1ac0c9081eeb1971b
Shajidaa <shajidaakterlopa101@gmail.com>
2025-12-07 09:53:17 +0600

    user api

--- index.js
+++ index.js
@@ -25,12 +25,30 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     const db = client.db("eduPlusDB");
+    const usersCollection = db.collection("users");
     const tuitionCollection = db.collection("tuitions");
 
+    //user api
+    app.post("/users", async (req, res) => {
+      try {
+        const userData = req.body;
+        userData.created_at = new Date().toString();
+        userData.last_loggedIn = new Date().toString();
+        const result = await usersCollection.insertOne(userData);
+        res.send(result);
+      } catch (error) {}
+    });
+
+    //tuitions apis
     app.post("/tuitions", async (req, res) => {
       const tuitionsData = req.body;
       console.log(tuitionsData);
       const result = await tuitionCollection.insertOne(tuitionsData);
+      res.send(result);
+    });
+
+    app.get("/tuitions", async (req, res) => {
+      const result = await tuitionCollection.find().toArray();
       res.send(result);
     });
 
