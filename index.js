4ddc4d0b1b51a73dc1e19b2f7546688f8d2a7653
Shajidaa <shajidaakterlopa101@gmail.com>
2025-12-07 15:51:01 +0600

    update api

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
 const express = require("express");
 var cors = require("cors");
+require("dotenv").config();
+
 const app = express();
 const port = 3000;
-
-// mongodb+srv://<db_username>:<db_password>@cluster0.xodph41.mongodb.net/?appName=Cluster0
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  "mongodb+srv://eduPlus:bSYLRujYfiGipESD@cluster0.xodph41.mongodb.net/?appName=Cluster0";
+const verifyJWT = async (req, res, next) => {
+  const token = req?.headers?.authorization?.split(" ")[1];
+  console.log(token);
+  if (!token) return res.status(401).send({ message: "Unauthorized Access!" });
+  try {
+    const decoded = await admin.auth().verifyIdToken(token);
+    req.tokenEmail = decoded.email;
+    console.log(decoded);
+    next();
+  } catch (err) {
+    console.log(err);
+    return res.status(401).send({ message: "Unauthorized Access!", err });
+  }
+};
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
+const client = new MongoClient(process.env.MONGODB_URI, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
@@ -34,11 +46,29 @@ async function run() {
         const userData = req.body;
         userData.created_at = new Date().toString();
         userData.last_loggedIn = new Date().toString();
+        const query = {
+          email: userData.email,
+        };
+        const alreadyExists = await usersCollection.findOne(query);
+        if (alreadyExists) {
+          const result = await usersCollection.updateOne(query, {
+            $set: {
+              last_loggedIn: new Date().toString(),
+            },
+          });
+          return res.send(result);
+        }
         const result = await usersCollection.insertOne(userData);
         res.send(result);
-      } catch (error) {}
+      } catch (error) {
+        res.status(500).json({ message: error.message });
+      }
     });
+    //user get by role
 
+    app.get("/user/role", async (req, res) => {
+      const result = await usersCollection.findOne();
+    });
     //tuitions apis
     app.post("/tuitions", async (req, res) => {
       const tuitionsData = req.body;
